21af47463d58156f7f0b9e80214c36def55fdd67
yangbingyan <yangbingyan159@163.com>
2017-10-15 10:12:55 +0800

    change segnet

--- segnet/segnet.py
+++ segnet/segnet.py
@@ -19,22 +19,22 @@ class Segnet:
         self.X_train = X_train
         self.y_train = y_train
 
-    def segnet(self):
+    def net(self):
 
-        Input_layer = Input(self.input.shape)
+        Input_layer = Input(self.X_train.shape)                    # (k,224,224,3)
 
         # encoder
         conv1 = convolutional.Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu')(Input_layer)
         bn = normalization.BatchNormalization()(conv1)
         conv1 = convolutional.Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu')(bn)
         bn = normalization.BatchNormalization()(conv1)
-        max_pooling1 = pooling.MaxPooling2D(pool_size=(2, 2))(bn)  # (112,112,3)
+        max_pooling1 = pooling.MaxPooling2D(pool_size=(2, 2))(bn)  # (k,112,112,64)
 
         conv2 = convolutional.Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu')(max_pooling1)
         bn = normalization.BatchNormalization()(conv2)
         conv2 = convolutional.Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu')(bn)
         bn = normalization.BatchNormalization()(conv2)
-        max_pooling2 = pooling.MaxPooling2D(pool_size=(2, 2))(bn)  # (56,56,3)
+        max_pooling2 = pooling.MaxPooling2D(pool_size=(2, 2))(bn)  # (k,56,56,128)
 
         conv3 = convolutional.Conv2D(filters=256, kernel_size=(3, 3), padding='same', activation='relu')(max_pooling2)
         bn = normalization.BatchNormalization()(conv3)
@@ -42,7 +42,7 @@ class Segnet:
         bn = normalization.BatchNormalization()(conv3)
         conv3 = convolutional.Conv2D(filters=256, kernel_size=(3, 3), padding='same', activation='relu')(bn)
         bn = normalization.BatchNormalization()(conv3)
-        max_pooling3 = pooling.MaxPooling2D(pool_size=(2, 2))(bn)  # (28,28,3)
+        max_pooling3 = pooling.MaxPooling2D(pool_size=(2, 2))(bn)  # (k,28,28,256)
 
         conv4 = convolutional.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(max_pooling3)
         bn = normalization.BatchNormalization()(conv4)
@@ -50,7 +50,7 @@ class Segnet:
         bn = normalization.BatchNormalization()(conv4)
         conv4 = convolutional.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(bn)
         bn = normalization.BatchNormalization()(conv4)
-        max_pooling4 = pooling.MaxPooling2D(pool_size=(2, 2))(bn)  # (14,14,3)
+        max_pooling4 = pooling.MaxPooling2D(pool_size=(2, 2))(bn)  # (k,14,14,512)
 
         conv5 = convolutional.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(max_pooling4)
         bn = normalization.BatchNormalization()(conv5)
@@ -58,10 +58,10 @@ class Segnet:
         bn = normalization.BatchNormalization()(conv5)
         conv5 = convolutional.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(bn)
         bn = normalization.BatchNormalization()(conv5)
-        max_pooling5 = pooling.MaxPooling2D(pool_size=(2, 2))(bn)  # (7,7,3)
+        max_pooling5 = pooling.MaxPooling2D(pool_size=(2, 2))(bn)  # (k,7,7,512)
 
         # decoder
-        upsample1 = convolutional.UpSampling2D(size=(2, 2))(max_pooling5)  # (14,14,3)
+        upsample1 = convolutional.UpSampling2D(size=(2, 2))(max_pooling5)  # (k,14,14,512)
         conv6 = convolutional.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(upsample1)
         bn = normalization.BatchNormalization()(conv6)
         conv6 = convolutional.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(bn)
@@ -69,7 +69,7 @@ class Segnet:
         conv6 = convolutional.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(bn)
         bn = normalization.BatchNormalization()(conv6)
 
-        upsample2 = convolutional.UpSampling2D(size=(2, 2))(bn)  # (28,28,3)
+        upsample2 = convolutional.UpSampling2D(size=(2, 2))(bn)  # (k,28,28,512)
         conv7 = convolutional.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(upsample2)
         bn = normalization.BatchNormalization()(conv7)
         conv7 = convolutional.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(bn)
@@ -77,7 +77,7 @@ class Segnet:
         conv7 = convolutional.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(bn)
         bn = normalization.BatchNormalization()(conv7)
 
-        upsample3 = convolutional.UpSampling2D(size=(2, 2))(bn)  # (56,56,3)
+        upsample3 = convolutional.UpSampling2D(size=(2, 2))(bn)  # (k,56,56,512)
         conv8 = convolutional.Conv2D(filters=256, kernel_size=(3, 3), padding='same', activation='relu')(upsample3)
         bn = normalization.BatchNormalization()(conv8)
         conv8 = convolutional.Conv2D(filters=256, kernel_size=(3, 3), padding='same', activation='relu')(bn)
@@ -85,19 +85,19 @@ class Segnet:
         conv8 = convolutional.Conv2D(filters=256, kernel_size=(3, 3), padding='same', activation='relu')(bn)
         bn = normalization.BatchNormalization()(conv8)
 
-        upsample4 = convolutional.UpSampling2D(size=(2, 2))(bn)  # (112,112,3)
+        upsample4 = convolutional.UpSampling2D(size=(2, 2))(bn)  # (k,112,112,256)
         conv9 = convolutional.Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu')(upsample4)
         bn = normalization.BatchNormalization()(conv9)
         conv9 = convolutional.Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu')(bn)
         bn = normalization.BatchNormalization()(conv9)
 
-        upsample5 = convolutional.UpSampling2D(size=(2, 2))(bn)  # (224,224,3)
+        upsample5 = convolutional.UpSampling2D(size=(2, 2))(bn)  # (k,224,224,128)
         conv10 = convolutional.Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu')(upsample5)
         bn = normalization.BatchNormalization()(conv10)
-        conv10 = convolutional.Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu')(bn)
+        conv10 = convolutional.Conv2D(filters=11, kernel_size=(3, 3), padding='same', activation='relu')(bn)
         bn = normalization.BatchNormalization()(conv10)
 
-        Output_layer = Dense(units=11, activation='softmax')(bn)
+        Output_layer = Activation('softmax')(bn)
 
         self.Input_layer = Input_layer
         self.Output_layer = Output_layer
@@ -110,12 +110,18 @@ class Segnet:
                       optimizer=sgd,
                       metrics=['accuracy'])
 
-    def net_visualization(self):
+    def net_visualization(self,graph_save=False):
+
+        if graph_save == True:
+            plot_model(self.model, to_file='model.png')
 
         SVG(model_to_dot(self.model, show_shapes=True).create(prog='dot', format='svg'))
 
-    def train(self):
+    def train(self,batch_size=64,epoch=5,verbose=2,):
 
-        self.history = self.model.fit(x=self.X_train, y=self.y_train, batch_size=512, epochs=120, verbose=2,
+        self.history = self.model.fit(x=self.X_train, y=self.y_train, batch_size=batch_size, epochs=epoch, verbose=verbose,
                   callbacks=[callbacks.ReduceLROnPlateau(patience=1, cooldown=10)])
 
+    def evaluate(self):
+
+        pass
